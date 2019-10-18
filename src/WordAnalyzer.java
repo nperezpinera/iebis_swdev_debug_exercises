eb7388e30ed8aef6554cdcfd375df9a17187825f
nperezp <nperezpinera@gmail.com>
2019-10-18 17:26:06 +0200

    updated word analyser

--- src/WordAnalyzer.java
+++ src/WordAnalyzer.java
@@ -1,3 +1,5 @@
+package com.company;
+
 /**
  * A class that analyzes words.
  */
@@ -19,7 +21,7 @@ public class WordAnalyzer {
      * @return the first repeated character, or 0 if none found
      */
     public char firstRepeatedCharacter() {
-        for (int i = 0; i < word.length(); i++) {
+        for (int i = 0; i < word.length()-1; i++) {
             char ch = word.charAt(i);
             if (ch == word.charAt(i + 1))
                 return ch;
@@ -37,14 +39,14 @@ public class WordAnalyzer {
     public char firstMultipleCharacter() {
         for (int i = 0; i < word.length(); i++) {
             char ch = word.charAt(i);
-            if (find(ch, i) >= 0)
-                return ch;
+                if (find(ch, i) >= 0)
+                    return ch;
         }
         return 0;
     }
 
     private int find(char c, int pos) {
-        for (int i = pos; i < word.length(); i++) {
+        for (int i = pos + 1; i < word.length(); i++) {
             if (word.charAt(i) == c) {
                 return i;
             }
@@ -60,7 +62,7 @@ public class WordAnalyzer {
      */
     public int countGroupsRepeatedCharacters() {
         int c = 0;
-        for (int i = 1; i < word.length() - 1; i++) {
+        for (int i = 0; i < word.length() - 1; i++) {
             if (word.charAt(i) == word.charAt(i + 1)) // found a repetition
             {
                 if (word.charAt(i - 1) != word.charAt(i)) // it't the start
@@ -71,4 +73,9 @@ public class WordAnalyzer {
     }
 
     private String word;
+
+    public static void main(String[] args) {
+        // write your code here
+    }
+
 }
