ce6fd402ccb5435fb968f0e6a3298b981b8de781
nperezp <nperezpinera@gmail.com>
2019-10-19 19:31:55 +0200

    Updated main.java for exercise 3, still working on 2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
+package com.company;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Main {
 
         public static void main(String [] args) {
-            double d = 0.0;
+            BigDecimal d = new BigDecimal(0.0);
+            BigDecimal a = new BigDecimal(1.0);
+            BigDecimal x = new BigDecimal(0.1);
 
-            while (d != 1.0) {
-                d += 0.1;
+            while (d.compareTo(a) != 0) {
+                BigDecimal sum = d.add(x);
+                d = sum.setScale(2, RoundingMode.FLOOR);
+
             }
 
             System.out.println("d is 1");
